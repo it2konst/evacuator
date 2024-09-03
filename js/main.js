5e3b09b35df49ac8b55fee6856aead025d7e7a90
Konstantin <163849606+it2konst@users.noreply.github.com>
2024-09-03 11:57:38 +0700

    fix: section evacuators

--- js/main.js
+++ js/main.js
@@ -1,32 +1,16 @@
 const swiper = new Swiper('.swiper', {
-	// direction: 'vertical',
 	loop: true,
 	spaceBetween: 32,
-	// slidesPerView: 2,
-	// slidesPerGroup: 2,
 	grabCursor: true,
-	// centeredSlides: true,
-	// initialSlide: 2,
 
 	pagination: {
 		el: '.swiper-pagination',
 		clickable: true,
-		// dynamicBullets: true,
 	},
-
 	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
+		nextEl: '.myslider-next',
+		prevEl: '.myslider-prev',
 	},
-
-	// scrollbar: {
-	// 	el: '.swiper-scrollbar',
-	// },
-
-	mousewheel: {
-		sensitivity: 1,
-	},
-
 	breakpoints: {
 		320: {
 			slidesPerView: 1,
@@ -36,14 +20,6 @@ const swiper = new Swiper('.swiper', {
 			slidesPerGroup: 2,
 			slidesPerView: 2,
 		},
-		// 992: {
-		// 	slidesPerView: 3,
-		// },
 	},
-
-	// zoom: {
-	// 	maxRatio: 5,
-	// 	minRatio: 1,
-	// },
 });
 

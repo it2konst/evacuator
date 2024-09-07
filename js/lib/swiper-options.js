057edfacd5d75a0f300452c9dccfade6418ac749
Konstantin <163849606+it2konst@users.noreply.github.com>
2024-09-07 21:22:43 +0700

    feat: add section - price

---
+++ js/lib/swiper-options.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper('.swiper', {
+	loop: true,
+	spaceBetween: 32,
+	grabCursor: true,
+
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: '.myslider-next',
+		prevEl: '.myslider-prev',
+	},
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			slidesPerGroup: 1,
+		},
+		1400: {
+			slidesPerGroup: 2,
+			slidesPerView: 2,
+			autoHeight: true,
+		},
+	},
+});
+
